1d7c8d6bc143e0b1b2846f16bd5dbc6f38bb88b9
Lokkee Loh <lkloh31@gmail.com>
2025-06-18 12:40:43 -0700

    convert string to array

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const userInputString = prompt(
   "Please enter some froyo flavors separated by commas.",
   "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
+
+const flavArray = convertStringToArray(userInputString);
+
+/**
+ * @param {string} commaSeparatedFlavors
+ * @returns {array[]} the string converted into an array of flavors
+ */
+function convertStringToArray(commaSeparatedFlavors) {
+  // Split the string of flavors into an array of strings.
+  const array = commaSeparatedFlavors.split(",");
+
+  const flavors = [];
+  for (const arr of array) {
+    flavors.push(arr);
+  }
+  return flavors;
+}
